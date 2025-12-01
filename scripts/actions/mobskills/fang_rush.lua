d9165fb06256c409c4a8cc954acf1f9f822dfdf8
Skold177 <113406182+Skold177@users.noreply.github.com>
2025-12-01 11:33:38 -0500

    Update fang_rush.lua

--- scripts/actions/mobskills/fang_rush.lua
+++ scripts/actions/mobskills/fang_rush.lua
@@ -15,7 +15,7 @@ end
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 3
     local accmod  = 1
-    local ftp     = 1
+    local ftp = skill:getScriptParam("ftp", 1)
     local params  = { canCrit = true }
     local info    = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, ftp, xi.mobskills.physicalTpBonus.NO_EFFECT, 0, 0, 0, params)
     local dmg     = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
