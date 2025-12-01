9676619c7413fb2cf5453f2a84e0f57c0c648231
Skold177 <113406182+Skold177@users.noreply.github.com>
2025-12-01 11:37:18 -0500

    Update lua_baseentity.h

--- src/map/lua/lua_baseentity.h
+++ src/map/lua/lua_baseentity.h
@@ -24,7 +24,7 @@
 
 #include "common/cbasetypes.h"
 #include "luautils.h"
-#include "packets/quest_mission_log.h"
+#include "enums/mission_log.h"
 #include "packets/s2c/0x009_message.h"
 #include "utils/battleutils.h"
 #include "utils/charutils.h"
